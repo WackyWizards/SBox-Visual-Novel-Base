e59b7f0792e0af7600bed7da924356d20fa96dde
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-13 20:28:26 +0300

    Add ScriptPlayer.HistoryEntry

--- Libraries/VNBase/Code/Systems/ScriptPlayer.History.cs
+++ Libraries/VNBase/Code/Systems/ScriptPlayer.History.cs
@@ -6,18 +6,30 @@ namespace VNBase;
 public sealed partial class ScriptPlayer
 {
 	/// <summary>
-	/// All previously shown labels.
+	/// All previously shown dialogue entries
 	/// </summary>
-	public List<Dialogue.Label> DialogueHistory { get; } = [];
-
-	private void AddHistory( Dialogue.Label label )
+	public List<HistoryEntry> DialogueHistory { get; } = [];
+    
+	/// <summary>
+	/// Adds a dialogue entry to history
+	/// </summary>
+	private void AddToHistory( string displayedText, Dialogue.Label label )
 	{
-		if ( DialogueHistory.Contains( label ) )
+		var historyEntry = new HistoryEntry
 		{
-			return;
-		}
+			Text = displayedText,
+			Label = label
+		};
 
-		label.Text = label.Text.Format( _environment );
-		DialogueHistory.Add( label );
+		DialogueHistory.Add( historyEntry );
+	}
+	
+	/// <summary>
+	/// Represents a single dialogue entry that was actually displayed to the player
+	/// </summary>
+	public readonly struct HistoryEntry( string text, Dialogue.Label label )
+	{
+		public string Text { get; init; } = text;
+		public Dialogue.Label Label { get; init; } = label;
 	}
 }
