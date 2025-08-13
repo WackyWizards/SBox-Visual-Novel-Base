73f7ca3ac4057cbce73dad63d061dafa3067aa13
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-13 22:07:19 +0300

    SubPanel properties are Parameters

--- Libraries/VNBase/Code/UI/SubPanel.cs
+++ Libraries/VNBase/Code/UI/SubPanel.cs
@@ -4,10 +4,13 @@ namespace VNBase.UI;
 
 public class SubPanel : Panel
 {
+	[Parameter]
 	public ScriptPlayer? Player { get; set; }
 	
+	[Parameter]
 	public Settings? Settings { get; set; }
 
+	[Parameter]
 	public VNHud? Hud { get; set; }
 
 	public void ToggleVisibility()
