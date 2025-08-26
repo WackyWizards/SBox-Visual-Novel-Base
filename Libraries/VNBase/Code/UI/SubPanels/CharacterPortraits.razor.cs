2007337b4c16e154648edf08783403695ad648dc
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-26 20:02:22 +0300

    Hide Portraits & Choices if there are none

--- Libraries/VNBase/Code/UI/SubPanels/CharacterPortraits.razor.cs
+++ Libraries/VNBase/Code/UI/SubPanels/CharacterPortraits.razor.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 namespace VNBase.UI;
 
 public partial class CharacterPortraits
 {
+#pragma warning disable CA1822
+	private bool HasCharacters => Player?.State.Characters.Any() == true;
+#pragma warning restore CA1822
+	
 	protected override int BuildHash()
 	{
-		return HashCode.Combine( Player?.State.DialogueText, Player?.State.Characters.Count );
+		return HashCode.Combine( Player?.State.DialogueText, Player?.State.Characters.Count, HasCharacters );
 	}
 }
--- Libraries/VNBase/Code/UI/SubPanels/Choices.razor.cs
+++ Libraries/VNBase/Code/UI/SubPanels/Choices.razor.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
 using VNScript;
 
 namespace VNBase.UI;
 
 public partial class Choices
 {
+#pragma warning disable CA1822
+	private bool HasChoices => Player?.State.Choices.Any() == true;
+#pragma warning restore CA1822
+	
 	private void ExecuteChoice( Script.Choice choice )
 	{
 		Player?.ExecuteChoice( choice );
@@ -12,6 +17,6 @@ public partial class Choices
 
 	protected override int BuildHash()
 	{
-		return HashCode.Combine( Player?.State.DialogueText, Player?.State.IsDialogueFinished, Player?.State.Choices.Count );
+		return HashCode.Combine( Player?.State.DialogueText, Player?.State.IsDialogueFinished, Player?.State.Choices.Count, HasChoices );
 	}
 }
