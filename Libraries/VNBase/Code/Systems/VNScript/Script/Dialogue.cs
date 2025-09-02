64c21e161c1e40263c0f9f5b133153c045666e73
kEllieDev <kEllieDev@users.noreply.github.com>
2025-09-02 17:49:44 +0300

    Add Script.Dialogue

---
+++ Libraries/VNBase/Code/Systems/VNScript/Script/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using VNBase.Assets;
+
+namespace VNScript;
+
+public partial class Script
+{
+	public class Dialogue
+	{
+		public FormattableText Text { get; set; } = string.Empty;
+
+		public Character? Speaker { get; set; }
+	}
+}
