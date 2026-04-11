4545342fb9a3ae432c1ff859bd922aea65258d1a
kellie <kellie@wackywizards.org>
2026-04-11 15:42:54 +0300

    Mark JsonPropertyName for these properties

--- Libraries/VNBase/Code/Systems/VNScript/Script/AfterLabel.cs
+++ Libraries/VNBase/Code/Systems/VNScript/Script/AfterLabel.cs
@@ -1,5 +1,6 @@
 ﻿using Sandbox;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace VNScript;
 
@@ -9,11 +10,14 @@ public partial class Script
 	{
 		public List<SParen> CodeBlocks { get; set; } = [];
 		
+		[JsonPropertyName( "isLastLabel" )]
 		public bool IsLastLabel { get; set; }
 		
 		[FilePath]
+		[JsonPropertyName( "scriptPath" )]
 		public string? ScriptPath { get; set; }
 		
+		[JsonPropertyName( "targetLabel" )]
 		public string? TargetLabel { get; set; }
 	}
 }
